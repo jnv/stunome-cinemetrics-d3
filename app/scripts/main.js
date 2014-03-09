f99f2a7374e6295d4ad39b77e0d28535d35db85e
Jan Vlnas <git@jan.vlnas.cz>
2014-03-10 00:46:27 +0100

    Copy arc.js verbatim, still lose gradients

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,103 +1,166 @@
 /*global d3: true */
 'use strict';
+
+//http://bl.ocks.org/mbostock/4341417
+
+(function()
+{
 /**
  * Generates radial isosceles trapezoid - like arc, bud with straight bases
  * Based on d3.svg.arc
  * https://github.com/mbostock/d3/blob/master/src/svg/arc.js
  */
 
-var ARC_OFFSET = -(Math.PI / 2); //-halfπ
-
-d3.svg.trapezoid = function(arc)
-{
-  var innerRadius = arc.innerRadius(),
-    outerRadius = arc.outerRadius(),
-    startAngle = arc.startAngle(),
-    endAngle = arc.endAngle();
-
-  var line = d3.svg.line();
-
-  function vertices(d)
+  d3.svg.trapezoid = function(arc)
   {
-    var r0 = d.innerRadius,
-        r1 = d.outerRadius,
+    var innerRadius = d3_svg_arcInnerRadius,
+      outerRadius = d3_svg_arcOuterRadius,
+      startAngle = d3_svg_arcStartAngle,
+      endAngle = d3_svg_arcEndAngle;
+
+    var line = d3.svg.line();
+
+    function vertices(d)
+    {
+      var r0 = innerRadius.apply(this, arguments),
+        r1 = outerRadius.apply(this, arguments),
         a0 = startAngle.apply(this, arguments) + ARC_OFFSET,
         a1 = endAngle.apply(this, arguments) + ARC_OFFSET,
-      // da = (a1 < a0 && (da = a0, a0 = a1, a1 = da), a1 - a0),
-      // df = da < Math.PI ? '0' : '1',
+        // da = (a1 < a0 && (da = a0, a0 = a1, a1 = da), a1 - a0),
+        // df = da < Math.PI ? '0' : '1',
         c0 = Math.cos(a0),
         s0 = Math.sin(a0),
         c1 = Math.cos(a1),
         s1 = Math.sin(a1);
 
-    return [ [r1 * c0, r1 * s0], [r1 * c1, r1 * s1], [r0 * c1, r0 * s1], [r0 * c0, r0 * s0]];
-  }
+      return [[r1 * c0, r1 * s0], [r1 * c1, r1 * s1], [r0 * c1, r0 * s1], [r0 * c0, r0 * s0]];
+    }
 
-  function trapezoid(d)
-  {
-    var points = vertices(d);
-    // console.log(points);
-    return line(points);
-  }
+    function trapezoid(d)
+    {
+      var points = vertices(d);
+      // console.log(points);
+      return line(points);
+    }
 
-  trapezoid.axis = function(d) {
-    var p = vertices(d),
+    trapezoid.axis = function(d)
+    {
+      var p = vertices(d),
         r0 = innerRadius.apply(this, arguments);
 
-    var x1 = (p[0][0] + p[1][0]) / 2,
+      var x1 = (p[0][0] + p[1][0]) / 2,
         y1 = (p[0][1] + p[1][1]) / 2;
 
-    var x2 = (p[2][0] + p[3][0]) / 2,
+      var x2 = (p[2][0] + p[3][0]) / 2,
         y2 = (p[2][1] + p[3][1]) / 2;
 
-    return [ [x1, y1], [x2, y2] ];
-  };
+      return [[x1, y1], [x2, y2]];
+    };
 
-  trapezoid.translate = function(d, volume) {
-    var a0 = startAngle.apply(this, arguments) + ARC_OFFSET,
+    trapezoid.translate = function(d, volume)
+    {
+      var a0 = startAngle.apply(this, arguments) + ARC_OFFSET,
         a1 = endAngle.apply(this, arguments) + ARC_OFFSET;
-    var middle = (a0 + a1) / 2;
-    var sx = Math.cos(middle),
+      var middle = (a0 + a1) / 2;
+      var sx = Math.cos(middle),
         sy = Math.sin(middle);
 
-    var v = volume;
+      var v = volume;
 
-    return [sx * v, sy * v];
+      return [sx * v, sy * v];
+    };
+
+    trapezoid.innerRadius = function(v) {
+      if (!arguments.length)
+        return innerRadius;
+      innerRadius = d3.functor(v);
+      return trapezoid;
+    };
+
+    trapezoid.outerRadius = function(v) {
+      if (!arguments.length)
+        return outerRadius;
+      outerRadius = d3.functor(v);
+      return trapezoid;
+    };
+
+    trapezoid.startAngle = function(v) {
+      if (!arguments.length)
+        return startAngle;
+      startAngle = d3.functor(v);
+      return trapezoid;
+    };
+
+    trapezoid.endAngle = function(v) {
+      if (!arguments.length) return endAngle;
+      endAngle = d3.functor(v);
+      return trapezoid;
+    };
+
+    return trapezoid;
   };
 
-  return trapezoid;
-};
+  var ARC_OFFSET = -(Math.PI / 2); //-halfπ
 
+  function d3_svg_arcInnerRadius(d)
+  {
+    return d.innerRadius;
+  }
 
-function toDeg(radians){
+  function d3_svg_arcOuterRadius(d)
+  {
+    return d.outerRadius;
+  }
+
+  function d3_svg_arcStartAngle(d)
+  {
+    return d.startAngle;
+  }
+
+  function d3_svg_arcEndAngle(d)
+  {
+    return d.endAngle;
+  }
+}());
+
+function toDeg(radians)
+{
   return radians * (180 / Math.PI);
 }
 
-var chapterColorRef = function(i) {
+var chapterColorRef = function(i)
+{
   return 'chapterColor' + i;
 };
 
-var colorGenerator = function(inChapters) {
+var colorGenerator = function(inChapters)
+{
   var chapters = inChapters;
 
-  function toRgb(colorSet) {
+  function toRgb(colorSet)
+  {
     return 'rgb(' + colorSet[0] + ',' + colorSet[1] + ',' + colorSet[2] + ')';
   }
 
 
-  function colorDefs() {
+  function colorDefs()
+  {
 
-    var totalFrames = function (previousVal, colorSet) {
+    var totalFrames = function(previousVal, colorSet)
+    {
       return previousVal + colorSet[3];
     };
 
-    var zipColors = function(total) {
-      return function(colorSet) {
+    var zipColors = function(total)
+    {
+      return function(colorSet)
+      {
         return [toRgb(colorSet), colorSet[3] / total];
       };
     };
 
-    return chapters.map(function(chapterColors){
+    return chapters.map(function(chapterColors)
+    {
       var total = chapterColors.reduce(totalFrames, 0);
       return chapterColors.map(zipColors(total));
     });
@@ -116,14 +179,12 @@ var movieChart = function(data)
   var dataset = data.durations;
   var motions = data.motions;
 
-  var rratio = 0.35;
-  var outerRadius = w * rratio;
-  var innerRadius = w * rratio / 2;
+  var outerRadius = Math.min(w, h) * .3 - 10;
+  var innerRadius = outerRadius * .6;
   var END_ANGLE = 1.8; // of 2Pi
 
   var motionmax = d3.max(motions);
-  var arc = d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius);
-  var trapezoid = d3.svg.trapezoid(arc);
+  var trapezoid = d3.svg.trapezoid();
   var pie = d3.layout.pie().endAngle(Math.PI * END_ANGLE).sort(null);
 
   var dataPie = pie(dataset);
@@ -141,7 +202,8 @@ var movieChart = function(data)
   var colorDefs = color.colorDefs();
 
   // TODO: this should be totally reimplemented with data bindings
-  for (var i = 0; i < colorDefs.length; i++) {
+  for (var i = 0; i < colorDefs.length; i++)
+  {
     // One gradient per chapter
     var chapter = colorDefs[i];
     var segment = dataPie[i];
@@ -149,36 +211,37 @@ var movieChart = function(data)
     var axis = trapezoid.axis(segment);
 
     var chapterDef = svgDefs.append('svg:linearGradient')
-        .attr('id', chapterColorRef(i))
-        .attr('gradientUnits', 'userSpaceOnUse')
-        .attr('x1', axis[0][0])
-        .attr('y1', axis[0][1])
-        .attr('x2', axis[1][0])
-        .attr('y2', axis[1][1]);
-        // .attr('gradientTransform', 'rotate('+ toDeg(rot) + ')');
+      .attr('id', chapterColorRef(i))
+      .attr('gradientUnits', 'userSpaceOnUse')
+      .attr('x1', axis[0][0])
+      .attr('y1', axis[0][1])
+      .attr('x2', axis[1][0])
+      .attr('y2', axis[1][1]);
+    // .attr('gradientTransform', 'rotate('+ toDeg(rot) + ')');
 
 
     var totalOffset = 0;
-    for(var j = 0; j < chapter.length; j++) {
+    for (var j = 0; j < chapter.length; j++)
+    {
       // Two stops per chapter: prevents color blending
       var colorSet = chapter[j];
       var currentColor = colorSet[0],
-          currentRatio = colorSet[1];
+        currentRatio = colorSet[1];
 
       chapterDef.append('svg:stop')
-            .attr('stop-color', currentColor)
-            .attr('offset', totalOffset);
+        .attr('stop-color', currentColor)
+        .attr('offset', totalOffset);
 
       totalOffset += currentRatio;
 
       chapterDef.append('svg:stop')
-            .attr('stop-color', currentColor)
-            .attr('offset', totalOffset);
+        .attr('stop-color', currentColor)
+        .attr('offset', totalOffset);
     }
   }
 
   var topGroup = svg.append('g')
-      .attr('transform', 'translate('+ w/2 + ',' + h/2 +')');
+    .attr('transform', 'translate(' + w / 2 + ',' + h / 2 + ')');
 
   //Set up groups
   var segmentGroups = topGroup.selectAll('g.segment')
@@ -194,46 +257,54 @@ var movieChart = function(data)
     return 'url(#' + chapterColorRef(i) + ')';
   }).attr('d', trapezoid)
     .transition()
-      //.delay(function(d) { return d * 40; })
-      .each(slide);
+  //.delay(function(d) { return d * 40; })
+  .each(slide);
 
-  function slide(d, i) {
+  function slide(d, i)
+  {
     var m = motions[i],
-        segment = d3.select(this);
+      segment = d3.select(this);
 
-    var tr = trapezoid.translate(d, m * innerRadius *3);
+    var tr = trapezoid.translate(d, m * innerRadius * 3);
 
 
-    (function repeat() {
+    (function repeat()
+    {
       segment.transition()
-          .ease('sin')
-          .duration(m*10000)
-          .attrTween('d', tweenTrapezoid(function(d, i) {
-            return {
-              innerRadius: (innerRadius + outerRadius) / 2,
-              outerRadius: (innerRadius + outerRadius)
-            };
-          }))
+        .ease('sin')
+        .duration(m * 10000)
+        .attrTween('d', tweenTrapezoid(function(d, i)
+        {
+          return {
+            innerRadius: (innerRadius + outerRadius) / 2,
+            outerRadius: (innerRadius + outerRadius)
+          };
+        }))
         .transition()
-          .attrTween('d', tweenTrapezoid(function(d, i) {
-            return {
-              innerRadius: innerRadius,
-              outerRadius: outerRadius
-            };
-          }))
-          .each('end', repeat);
+        .attrTween('d', tweenTrapezoid(function(d, i)
+        {
+          return {
+            innerRadius: innerRadius,
+            outerRadius: outerRadius
+          };
+        }))
+        .each('end', repeat);
     })();
   }
 
 
-  function tweenTrapezoid(b) {
-    return function(a, i) {
+  function tweenTrapezoid(b)
+  {
+    return function(a, i)
+    {
       var d = b.call(this, a, i),
-          i = d3.interpolate(a, d);
-      for (var k in d) {
+        i = d3.interpolate(a, d);
+      for (var k in d)
+      {
         a[k] = d[k]; // update received data
       }
-      return function(t) {
+      return function(t)
+      {
         return trapezoid(i(t));
       };
     };
